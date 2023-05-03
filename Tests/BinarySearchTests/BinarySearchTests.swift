1dfce29e0649c0ff4b02326965c4a38b35877c50
David Monagle <dmonagle@users.noreply.github.com>
2023-05-03 14:56:03 +1000

    Initial Commit

---
+++ Tests/BinarySearchTests/BinarySearchTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import BinarySearch
+
+final class BinarySearchTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(BinarySearch().text, "Hello, World!")
+    }
+}
